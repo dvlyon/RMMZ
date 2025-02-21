4666b60c31872d4cea74b3ff1f5fc492ac6d626a
David Lyon <davidlyon1988@gmail.com>
2025-02-21 17:20:40 +0100

    Stat tracking

--- Utils/StatTracker/DvLyon_StatsTracker.js
+++ Utils/StatTracker/DvLyon_StatsTracker.js
@@ -53,13 +53,22 @@ Follow us on socials:
 // Constants
 //=============================================================================
 
-const DefaultStats = {
-	Moves: 0,
-};
-
 //=============================================================================
 // Helpers
 //=============================================================================
+
+function makeDefaultStats() {
+	return {
+		Steps: {
+			p: 0,
+		},
+		Encounters: {
+			// Predefine some keys so they’re always objects
+			t: {},
+			p: {},
+		},
+	};
+}
 
 //=============================================================================
 // Declarations
@@ -85,30 +94,120 @@ const DefaultStats = {
 	// Managers
 	//=============================================================================
 
+	// BattleManager
+
+	const _BattleManager_endBattle = BattleManager.endBattle;
+    BattleManager.endBattle = function(result) {
+        _BattleManager_endBattle.call(this, result);
+
+        // Tally a total encounter result
+        $gameDvLyon.addEncounter(result);
+
+        // For each enemy, log party-level and individual actor stats
+        $gameTroop.members().forEach(enemy => {
+            $gameDvLyon.addPartyEncounter(enemy._enemyId, result);
+            $gameParty.battleMembers().forEach(member => {
+                $gameDvLyon.addMemberEncounter(member._actorId, enemy._enemyId, result);
+            });
+        });
+    };
+
 	//=============================================================================
 	// Objects
 	//=============================================================================
 
 	// Game_DvLyon
 
-	const _Game_DvLyon_initialize = Game_DvLyon.prototype.initialize
-	Game_DvLyon.prototype.initialize = function() {
-		_Game_DvLyon_initialize.call(this)
-		this.StatsTracker = this.StatsTracker ?? DefaultStats
-	}
+	const _Game_DvLyon_initialize = Game_DvLyon.prototype.initialize;
+    Game_DvLyon.prototype.initialize = function() {
+        _Game_DvLyon_initialize.call(this);
+        
+        // If it’s undefined or null, give it a brand new default structure:
+        if (!this.StatsTracker) {
+            this.StatsTracker = makeDefaultStats();
+        }
+    };
 
-	Game_DvLyon.prototype.addMove = function() {
-		this.StatsTracker.Moves += 1
-	}
+	/**
+     * Just for debugging/logging in the console
+     */
+    Game_DvLyon.prototype.showStats = function() {
+        console.log(this.StatsTracker);
+    };
+
+	/**
+     * Count steps for a given actor or for the entire party.
+     */
+    Game_DvLyon.prototype.addStep = function(characterIndex, party = false) {
+        // Initialize if missing
+        if (!this.StatsTracker.Steps[characterIndex]) {
+            this.StatsTracker.Steps[characterIndex] = 0;
+        }
+        // Increase steps for that specific character
+        this.StatsTracker.Steps[characterIndex]++;
+
+        // If counting party steps:
+        if (party) {
+            if (!this.StatsTracker.Steps['p']) {
+                this.StatsTracker.Steps['p'] = 0;
+            }
+            this.StatsTracker.Steps['p']++;
+        }
+    };
+
+	/**
+     * Total encounters: track the result (win, escape, lose, etc.) globally.
+     */
+    Game_DvLyon.prototype.addEncounter = function(result) {
+        // Make sure .Encounters.t is an object
+        if (!this.StatsTracker.Encounters.t) {
+            this.StatsTracker.Encounters.t = {};
+        }
+        if (!this.StatsTracker.Encounters.t[result]) {
+            this.StatsTracker.Encounters.t[result] = 0;
+        }
+        this.StatsTracker.Encounters.t[result]++;
+    };
+
+	/**
+     * Party encounters vs a particular enemy ID.
+     */
+    Game_DvLyon.prototype.addPartyEncounter = function(enemyId, result) {
+		this.addMemberEncounter('p', enemyId, result)
+    };
+
+    /**
+     * Encounters for a specific actor against a particular enemy.
+     */
+    Game_DvLyon.prototype.addMemberEncounter = function(actorId, enemyId, result) {
+        // Make sure .Encounters[actorId] and .Encounters[actorId][enemyId] exist
+        if (!this.StatsTracker.Encounters[actorId]) {
+            this.StatsTracker.Encounters[actorId] = {};
+        }
+        if (!this.StatsTracker.Encounters[actorId][enemyId]) {
+            this.StatsTracker.Encounters[actorId][enemyId] = {};
+        }
+        if (!this.StatsTracker.Encounters[actorId][enemyId][result]) {
+            this.StatsTracker.Encounters[actorId][enemyId][result] = 0;
+        }
+        this.StatsTracker.Encounters[actorId][enemyId][result]++;
+    };
 
 	// Game_Player
 
+	const _Game_Player_increaseSteps = Game_Player.prototype.increaseSteps
 	Game_Player.prototype.increaseSteps = function() {
-		Game_Character.prototype.increaseSteps.call(this);
-		if (this.isNormal()) {
-			$gameParty.increaseSteps();
-		}
-	};
+		_Game_Player_increaseSteps.call(this)
+		$gameDvLyon.addStep($gameParty.leader()._actorId, true)
+	}
+
+	// Game_Follower
+
+	const _Game_Follower_increaseSteps = Game_Follower.prototype.increaseSteps
+	Game_Follower.prototype.increaseSteps = function() {
+		_Game_Follower_increaseSteps.call(this)
+		$gameDvLyon.addStep(this.actor()._actorId)
+	}
 
 	//=============================================================================
 	// Scenes
